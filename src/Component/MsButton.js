ed95b153324362943f6726d58d4e89e4dc0dc2a3
dalroy44 <dalroy44@gmail.com>
2019-02-18 16:58:02 -0500

    Initial commit

---
+++ src/Component/MsButton.js
@@ -0,0 +1,15 @@
+import React from "react";
+import "antd/dist/antd.css";
+import { Button } from "antd";
+
+class MsButton extends React.Component {
+  render() {
+    return (
+      <div>
+        <Button type="primary">Primary</Button>
+      </div>
+    );
+  }
+}
+
+export default MsButton;
